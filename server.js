2df298130aff4449c4f6798e7a14f816bbd607bf
bzhseo <francois.mouton@gmail.com>
2018-02-20 20:05:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 
 // config
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 // routes
 app.get('/', function(req, res) {
