f4926c08a7c7b4ce2eeef415c76c6a5d4d641d3e
bzhseo <francois.mouton@gmail.com>
2018-03-05 17:44:28 +0000

    Reinit app build with express

--- server.js
+++
@@ -1,7 +0,0 @@
-var mustacheExpress = require('mustache-express');
-app.engine('html', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
-app.get('/', function (req, res) {
-    res.render('layout');
-});
