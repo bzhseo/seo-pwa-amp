221aa4b29f373626427c17a0ad619333804a5382
bzhseo <francois.mouton@gmail.com>
2018-03-05 13:33:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-// Template
-const Mustache = require('mustache');
-const { resolve } = require('path');
-const layout = require('fs').readFileSync(
-	resolve(__dirname, 'layout.html'),
-	'utf8'
-);
-
-// http server
-const express = require('express');
-const app = express();
-
-// config
-const port = process.env.PORT || 8080;
-
-// routes
-app.get('/', function(req, res) {
-	res.send(Mustache.render(layout, req));
+var mustacheExpress = require('mustache-express');
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.get('/', function (req, res) {
+    res.render('layout');
 });
-
-app.listen(port, () => console.log('Example app listening on port %s!', port));
